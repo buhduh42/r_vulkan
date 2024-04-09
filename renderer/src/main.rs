688fec5fbc3fa968b9757a327e50a2a170364c97
Dale Owens <dale.owens42@gmail.com>
2024-04-08 23:04:32 -0700

    got a winit window running...maybe, not really sure

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -1,33 +1,6 @@
-pub mod system;
+pub mod window;
 
 fn main() {
-    let event_loop = system::event_loop::EventLoop::new(None)
-        .unwrap();
-    system::init(1920, 1080, &event_loop);
-    event_loop.run();
+    let window = window::Window::new(1920, 1080, None).unwrap();
+    let _ = window.render_loop(||()).unwrap();
 }
-
-/*
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
-
-fn main() {
-    //let mut event_loop = EventLoop::new().unwrap();
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Wait);
-    let _window = Window::new(&event_loop).unwrap();
-
-    let _ = event_loop.run(move |event, elwt| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => elwt.exit(),
-            _ => (),
-        }
-    });
-}
-*/
---
+++ renderer/src/window.rs
@@ -0,0 +1,93 @@
+use std::{
+    error::Error, 
+    time::Instant,
+    cell::RefCell,
+};
+
+use winit::{
+    keyboard::{
+        Key as SystemKey,
+        NamedKey as SystemNamedKey,
+    },
+    event::{
+        Event as SystemEvent,
+        WindowEvent as SystemWindowEvent,
+        KeyEvent as SystemKeyEvent,
+        ElementState as SystemElementState,
+    },
+    event_loop::{
+        EventLoop as SystemEventLoop,
+        ControlFlow as SystemControlFlow,
+    },
+    window::{
+        Window as SystemWindow,
+        WindowBuilder as SystemWindowBuilder,
+    },
+    platform::run_on_demand::EventLoopExtRunOnDemand,
+};
+
+#[derive(Default)]
+pub enum LoopStyle {
+    #[default]
+    Poll,
+    Wait,
+    WaitUntil(Instant),
+}
+
+pub struct Window {
+    window: SystemWindow,
+    pub event_loop: RefCell<SystemEventLoop<()>>,
+}
+
+impl Window {
+    pub fn new(window_width: u32, window_height: u32, loop_style: Option<LoopStyle>) 
+            -> Result<Self, Box<dyn Error>> 
+    {
+        let event_loop = SystemEventLoop::new()?;
+        let control_flow = match loop_style.unwrap_or_default() {
+            LoopStyle::Poll => SystemControlFlow::Poll,
+            LoopStyle::Wait => SystemControlFlow::Wait,
+            LoopStyle::WaitUntil(instant) => SystemControlFlow::WaitUntil(instant),
+        };
+        event_loop.set_control_flow(control_flow);
+        let window = SystemWindowBuilder::new()
+            .with_title("Ash - Example")
+            .with_inner_size(winit::dpi::LogicalSize::new(
+                f64::from(window_width),
+                f64::from(window_height),
+            ))
+            .build(&event_loop)
+            .unwrap();
+        Ok(Self { 
+            window: window, 
+            event_loop: RefCell::new(event_loop),
+        })
+    }
+
+    //ripped directly from /home/dale/third_party/ash/ash-examples/src/bin/texture.rs
+    pub fn render_loop<F: Fn()>(&self, f: F) -> Result<(), impl Error> {
+        self.event_loop.borrow_mut().run_on_demand(|event, elwp| {
+            elwp.set_control_flow(SystemControlFlow::Poll);
+            match event {
+                SystemEvent::WindowEvent {
+                    event:
+                        SystemWindowEvent::CloseRequested
+                        | SystemWindowEvent::KeyboardInput {
+                            event:
+                                SystemKeyEvent {
+                                    state: SystemElementState::Pressed,
+                                    logical_key: SystemKey::Named(SystemNamedKey::Escape),
+                                    ..
+                                },
+                            ..
+                        },
+                    ..
+                } => {
+                    elwp.exit();
+                }
+                SystemEvent::AboutToWait => f(),
+                _ => (),
+            }
+        })
+    }
+}
